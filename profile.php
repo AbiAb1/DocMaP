6780642c9b6d89da0c65a6a4657fa235e6aa843d
Jamir Hernandez <21-77830@g.batstate-u.edu.ph>
2024-12-18 21:44:18 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -304,7 +304,7 @@ mysqli_close($conn);
     </style>
 </head>
 
-<body  style="background-color#f0f0f0;">
+<body   style="background-color:#f0f0f0;">
     <!-- SIDEBAR -->
     <section id="sidebar">
         <?php include 'navbar.php'; ?>
