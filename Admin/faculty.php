f39261c82be720d174947f7350e53b89b4a10990
Jamir Hernandez <21-77830@g.batstate-u.edu.ph>
2024-12-18 22:35:01 +0800

    Update faculty.php

--- Admin/faculty.php
+++ Admin/faculty.php
@@ -521,7 +521,7 @@ $conn->close();
 
     </style>
 </head>
-<body>
+<body style="background-color:#f0f0f0;">
     <!-- SIDEBAR -->
     <section id="sidebar">
         <?php include 'navbar.php'; ?>
