ec8c4164fc3a086f37b20663ac924de3066df03f
Christian Abiog <21-78797@g.batstate-u.edu.ph>
2025-01-09 17:15:33 +0800

    Update mps.php

--- Admin/mps.php
+++ Admin/mps.php
@@ -48,8 +48,7 @@ if ($resultQuarter) {
 }
 $queryMPS = "SELECT m.mpsID, m.UserID, m.ContentID, q.School_Year_ID, q.Quarter_Name, 
                     CONCAT(fc.Title, ' - ', fc.Captions) AS GradeSection, 
-                    m.TotalNumOfStudents, m.TotalNumTested, m.HighestScore, 
-                    m.LowestScore, m.MPS,sy.Year_Range AS SY,
+                    m.TotalNumOfStudents, m.TotalNumTested, m.MPS,sy.Year_Range AS SY,
                     CONCAT(ua.fname, ' ', ua.lname) AS SubTeacher
              FROM mps m
              INNER JOIN quarter q ON m.Quarter_ID = q.Quarter_ID
