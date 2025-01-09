72950c1f252e647b4acc744c78382f3e52e927af
Christian Abiog <21-78797@g.batstate-u.edu.ph>
2025-01-09 18:58:54 +0800

    Update templates.php

--- Admin/templates.php
+++ Admin/templates.php
@@ -166,7 +166,7 @@ $rowCount = mysqli_num_rows($result); // Get the number of rows returned
                                     <td><?php echo htmlspecialchars($row['created_at']); ?></td>
                                     <td>
                                        <!-- View Icon -->
-                                        <a href="https://htmlpreview.github.io/?https://raw.githubusercontent.com/AbiAb1/DocMaP/extra/Admin/Templates/<?php echo $row['filename']; ?>" target="_blank" class="btn btn-circle btn-view" title="View">
+                                        <a href="https://raw.githubusercontent.com/AbiAb1/DocMaP/extra/Admin/Templates/<?php echo $row['filename']; ?>" target="_blank" class="btn btn-circle btn-view" title="View">
                                             <i class="fas fa-eye"></i>
                                         </a>
                                         <!-- Delete Icon -->
